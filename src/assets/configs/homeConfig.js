c75103e8575b3257890b8199338f901b1d021ac0
Vishal Raj <vishalraj247@gmail.com>
2023-06-16 17:08:37 +0530

    Update homeConfig.js

--- src/assets/configs/homeConfig.js
+++ src/assets/configs/homeConfig.js
@@ -24,11 +24,11 @@ const homeConfig = {
             id: "work-3",
             title: "Data Science Intern",
             company: "University of Technology Sydney",
+            date: "Mar 2023 - May 2023",
             description: "Engaged in a research project, I worked extensively with the CoAuthor dataset, " +
             "analysing keystroke-level data of users co-writing with GPT-3. My role involved conducting literature reviews, creating " +
             "visualisations, defining user behavioural metrics, and extending Python notebooks for NLP reports. I also developed a " +
             "dashboard to highlight key findings and provide valuable feedback to educators.",
-            date: "Mar 2023 - May 2023",
             icon: <MdOutlineWorkOutline/>,
             tags: ["Data Science", "Research", "CoAuthor Dataset", "Keystroke-level Data", "GPT-3", "Literature Review", 
                    "Data Visualisation", "User Behavior Metrics", "Python", "Natural Language Processing (NLP)", "Data Storytelling",
@@ -39,12 +39,12 @@ const homeConfig = {
             id: "work-2",
             title: "MSc Data Science",
             company: "University of Technology Sydney",
+            date: "Feb 2022 - Dec 2023",
             description: "Pursuing a Master's in Data Science and Innovation, I've gained hands-on experience in solving business " +
             "problems using the CRISP-DM approach. My academic journey has involved developing various machine-learning models, " +
             "building Monte Carlo simulation models, and performing textual analysis using NLP techniques. I've honed my skills " +
             "in Python, handling class imbalance, and conducting sentiment analysis, " +
             "including the construction of ETLs, data lakes, and data warehouses.",
-            date: "Feb 2022 - Dec 2023",
             icon: <MdHomeWork/>,
             tags: ["Data Science", "CRISP-DM", "Machine Learning", "Python", "Regression Models", "Classifiers", "Monte Carlo Simulation",
                    "Textual Analysis", "NLP", "Sentiment Analysis", "ml", "airflow", "python", "docker", "snowflake", "postgres", "azure",
@@ -54,10 +54,10 @@ const homeConfig = {
             id: "work-1",
             title: "Data Analytics Professional Certificate",
             company: "Google/Coursera",
+            date: "May 2021 - Aug 2021",
             description: "Acquired a Data Analytics Professional Certificate from Google/Coursera, where I developed key analytical " +
             "skills, including data aggregation, data ethics, and visualisation in Tableau. I also gained proficiency in tools such as " +
             "spreadsheets, SQL, R Studio, and Tableau.",
-            date: "May 2021 - Aug 2021",
             icon: <GrCertificate/>,
             tags: ["Data Analytics", "Google", "Coursera", "Data Aggregation", "Data Ethics", "Visualization", "Tableau", "Spreadsheets",
                    "SQL", "R Studio"]
@@ -66,10 +66,10 @@ const homeConfig = {
             id: "work-0",
             title: "Data Science Specialization Professional Certificate",
             company: "IBM/Coursera",
+            date: "Oct 2020 - Apr 2021",
             description: "Completed a Data Science Specialization Professional Certificate from IBM/Coursera, where I honed key skills " +
             "such as data analysis, Python programming, statistical analysis, and data visualisation. I also became adept at using " + 
             "tools like Jupyter Notebook, GitHub, and Watson Studio.",
-            date: "Oct 2020 - Apr 2021",
             icon: <GrCertificate/>,
             tags: ["Data Science", "IBM", "Coursera", "Data Analysis", "Python", "Statistical Analysis", "Data Visualization", 
                    "Jupyter Notebook", "GitHub", "Watson Studio"]
